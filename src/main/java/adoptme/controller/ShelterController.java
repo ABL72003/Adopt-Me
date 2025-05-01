5366f2a96731a20e64b74151bc2845b1590ff248
Logan Anderson <ABL72003@gmail.com>
2025-05-01 14:35:04 -0500

    Refactor MVC structure, move Main to view, add ShelterController

---
+++ src/main/java/adoptme/controller/ShelterController.java
@@ -0,0 +1,16 @@
+package adoptme.controller;
+
+import adoptme.*;
+import adoptme.model.Pet;
+import adoptme.model.Shelter;
+import adoptme.view.Main;
+import com.google.gson.Gson;
+
+import java.awt.event.*;
+import java.util.List;
+
+public class ShelterController {
+
+	private Shelter<Pet> shelter;
+    private MainWindow view;
+}
