9d5dd8795b144cf1b457c9f08f6fb35897e7d2c6
Alexis <alexispaten@gmail.com>
2025-04-30 23:41:16 -0500

    Added rabbit subclass that extends pet

---
+++ src/main/java/adoptme/controller/pets/Rabbit.java
@@ -0,0 +1,39 @@
+package adoptme.controller.pets;
+
+public class Rabbit extends Pet{
+	/**
+	 * Constructor for files
+	 * @param name
+	 * @param age
+	 * @param species
+	 * @param id
+	 * @param adopted
+	 */
+	public Rabbit(String name, int age, String species, int id, boolean adopted) {
+		
+		super(name, age, species, id, adopted); // Calls Pet's constructor
+	}
+	
+	/**
+	 * Constructor for users
+	 * @param name
+	 * @param age
+	 * @param species
+	 * @param adopted
+	 */
+	public Rabbit(String name, int age, String species, boolean adopted) {
+		
+		super(name, age, species, adopted); // Calls Pet's constructor
+	}
+	
+	@Override
+	public String getType() {
+		return "Rabbit";
+	}
+	
+	@Override
+	public String toString() {
+	    return "[ID: " + getID() + ", Name:" + getName() + ", Age: " + getAge() + ", Species: "
+	    		+ getSpecies() + ", Adopted: " + getAdoptionStatus() + ", Type: " + getType() + "]";
+	}
+}
