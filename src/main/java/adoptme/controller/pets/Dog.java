9cfa265cebbb6dd6e8513cdb58ffc06dc1b7c713
Alexis <alexispaten@gmail.com>
2025-04-30 23:41:00 -0500

    Finished Dog subclass

--- src/main/java/adoptme/controller/pets/Dog.java
+++ src/main/java/adoptme/controller/pets/Dog.java
@@ -2,21 +2,39 @@ package adoptme.controller.pets;
 
 public class Dog extends Pet{
 	
+	/**
+	 * Constructor for files
+	 * @param name
+	 * @param age
+	 * @param species
+	 * @param id
+	 * @param adopted
+	 */
 	public Dog(String name, int age, String species, int id, boolean adopted) {
 		
 		super(name, age, species, id, adopted); // Calls Pet's constructor
 	}
-
+	
+	/**
+	 * Constructor for users
+	 * @param name
+	 * @param age
+	 * @param species
+	 * @param adopted
+	 */
+	public Dog(String name, int age, String species, boolean adopted) {
+		
+		super(name, age, species, adopted); // Calls Pet's constructor
+	}
+	
 	@Override
 	public String getType() {
-		// TODO Auto-generated method stub
-		return null;
+		return "Dog";
 	}
-
+	
 	@Override
-	public void setType(String type) {
-		// TODO Auto-generated method stub
-		
+	public String toString() {
+	    return "[ID: " + getID() + ", Name:" + getName() + ", Age: " + getAge() + ", Species: "
+	    		+ getSpecies() + ", Adopted: " + getAdoptionStatus() + ", Type: " + getType() + "]";
 	}
-
 }
